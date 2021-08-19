043bc7ee05e3af019b5726057d255d10ca46df5a
JoaoCarvalho987 <64854483+JoaoCarvalho987@users.noreply.github.com>
2021-08-19 16:26:49 -0300

    Add files via upload

---
+++ lojaetec.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19-Ago-2021 às 18:25
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `lojaetec`
+--
+CREATE DATABASE IF NOT EXISTS `lojaetec` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `lojaetec`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idcli` int(11) NOT NULL,
+  `nomecli` varchar(50) NOT NULL,
+  `endcli` varchar(100) DEFAULT NULL,
+  `fonecli` varchar(15) NOT NULL,
+  `emailcli` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`idcli`, `nomecli`, `endcli`, `fonecli`, `emailcli`) VALUES
+(1, 'lk', 'rua', '11964825341', 'lk@gmail.com'),
+(2, 'hec', 'bairro', '11978425361', 'hec@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ordem_servico`
+--
+
+CREATE TABLE `ordem_servico` (
+  `idOrdem` int(11) NOT NULL,
+  `data_os` timestamp NOT NULL DEFAULT current_timestamp(),
+  `equipamento` varchar(150) NOT NULL,
+  `defeito` varchar(150) NOT NULL,
+  `servico` varchar(150) DEFAULT NULL,
+  `tecnico` varchar(30) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `idcli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `ordem_servico`
+--
+
+INSERT INTO `ordem_servico` (`idOrdem`, `data_os`, `equipamento`, `defeito`, `servico`, `tecnico`, `valor`, `idcli`) VALUES
+(1, '2021-08-27 19:28:19', 'placa de vídeo', 'queimou', 'trocar', 'bla', '145.00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUser` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `fone` varchar(15) DEFAULT NULL,
+  `email` varchar(15) NOT NULL,
+  `senha` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUser`, `nome`, `fone`, `email`, `senha`) VALUES
+(1, 'Admin', '11978462351', 'admin@gmail.com', 'admin'),
+(2, 'Admin2', '11978463151', 'admin2@gmail.co', 'admin'),
+(4, 'jui', '11643512784', 'jui@jui.com', 'jui');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idcli`);
+
+--
+-- Índices para tabela `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  ADD PRIMARY KEY (`idOrdem`),
+  ADD KEY `idcli` (`idcli`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idcli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  MODIFY `idOrdem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  ADD CONSTRAINT `ordem_servico_ibfk_1` FOREIGN KEY (`idcli`) REFERENCES `clientes` (`idcli`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
